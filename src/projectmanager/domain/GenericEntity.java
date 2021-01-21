ab8dfdaef53c7e75be85b8240a472fac7431e5fa
emapiric998 <emapiric998@gmail.com>
2021-01-21 16:51:51 +0100

    Generic entity

---
+++ src/projectmanager/domain/GenericEntity.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectmanager.domain;
+
+import java.io.Serializable;
+
+public interface GenericEntity extends Serializable {
+
+    String getTableName();
+
+    String getColumnNamesForInsert();
+
+    String getInsertValues();
+
+    void setId(Long id);
+}
